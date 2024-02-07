55a778963657217a0501de023b04942d8359d0f4
Peter Sankauskas <pas256@gmail.com>
2024-02-07 11:23:59 -0800

    Add line items without Turbo

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   resources :quotes do
-    resources :line_item_dates, except: [:index, :show]
+    resources :line_item_dates, except: [:index, :show] do
+      resources :line_items, except: [:index, :show]
+    end
   end
 end
